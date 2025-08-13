2d55b3b67e40a740593319e01e0ccca8a29869e8
Paul Miyamoto <mfsaru@me.com>
2025-08-13 15:33:47 +0900

    added a visitor can add a new review to a restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    resources :review, only: [:new, :create]
+    resources :reviews, only: [:new, :create]
   end
   resources :reviews, only: [:destroy]
 end
